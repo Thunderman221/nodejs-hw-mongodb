88e7311191fd46799d4d5ef58ffc6d228d7b994b
Thunderman221 <119857501+Thunderman221@users.noreply.github.com>
2024-12-22 14:22:58 +0200

    Fix some errors_07

--- src/server.js
+++ src/server.js
@@ -34,14 +34,7 @@ const setupServer = async () => {
         },
       }),
     );
-    app.use(
-      '/api-docs',
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerDocument, {
-        explorer: true,
-        tagsSorter: 'alpha',
-      }),
-    );
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     app.use('/contacts', contactsRouter);
     app.use('/auth', authRouter);
